90104d4b8ad82656b9ac7866481af86d5cbde808
FN Otaibi <nsayemlil@hotmail.com>
2020-07-15 15:38:36 +0300

    added watched to the list

--- src/movies.js
+++ src/movies.js
@@ -2,21 +2,25 @@ const movies = [
   {
     id: 1,
     name: "Mulan",
+    watched:
   },
 
   {
     id: 2,
     name: "Mulan 2",
+    watched:
   },
 
   {
     id: 3,
     name: "Home Alone",
+    watched:
   },
 
   {
     id: 4,
     name: "Baby's day out",
+    watched:
   },
 ];
 
